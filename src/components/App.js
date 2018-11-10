3264eb17ac395cebb97fd94fba0ac1fb7de35f73
briantam23 <briantam23@gmail.com>
2018-11-10 14:02:42 -0500

    cleaned up code, css

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component, Fragment } from 'react';
-import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import AppBar from '@material-ui/core/AppBar';
 
 class App extends Component {
     render() {
         return(
             <Fragment>
-                <hr/>
-                <Button variant='contained' color='primary'>Test</Button>
+                <AppBar>Air Quality Index</AppBar>
                 <img src='../../public/airQualityLevels.png' alt='Air Quality Levels Guide'/>
+                <Input id='autocomplete' placeholder='Enter a location!' type='text' fullWidth/>
             </Fragment>
         )
     }
